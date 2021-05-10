fdb08a038707acbe64da5bb0b50af4557c93e644
dinesh-skyach <dinesh.chohda@skyach.com>
2021-05-10 19:19:22 +0530

    Added test case and changed code according to the comments

--- app/api/v2/admin/import_configs.rb
+++ app/api/v2/admin/import_configs.rb
@@ -1,20 +1,18 @@
 # encoding: UTF-8
 # frozen_string_literal: true
 
-require 'peatio/import_blockchain_currency_wallet'
-
 module API
   module V2
     module Admin
-      class ImportBlockchainCurrencyWallets < Grape::API
+      class ImportConfigs < Grape::API
 
         desc 'Import currency, blockchain and wallet from json file'
         params do
           requires :file,
                    type: File
         end
-        post '/import_blockchain_currency_wallets' do
-          Peatio::ImportBlockchainCurrencyWallet.new.process(params)
+        post '/import_configs' do
+          API::V2::ImportConfigHelpers.new.process(params)
           present(result: 'Importing of currency, blockchain and wallet is starting')
         end
       end
