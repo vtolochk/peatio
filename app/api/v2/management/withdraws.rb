f091619a209a7e3c0cf96e0685c3fddc81380c48
Valentyn Tolochko <vtolochk@gmail.com>
2021-08-21 10:28:18 +0300

    add: adds reject action decription

--- app/api/v2/management/withdraws.rb
+++ app/api/v2/management/withdraws.rb
@@ -156,6 +156,7 @@ module API
           @settings[:scope] = :write_withdraws
           detail '«process» – system will lock the money, check for suspected activity, validate recipient address, and initiate the processing of the withdraw. ' \
                 '«cancel»  – system will mark withdraw as «canceled», and unlock the money.' \
+                '«reject»  – system will mark withdraw as «rejected», and unlock the money.' \
                 '«review»  – system will mark withdraw as «under_review», and lock the money.' \
                 '«success»  – system will mark withdraw as «succeed», and subtract the money from the account. (works only with fiat)'
           success API::V2::Management::Entities::Withdraw
